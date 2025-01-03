3d774d9559f29c1b33c1c7ad932592b30cdf8947
s3warriors <satveerswami982@gmail.com>
2025-01-03 15:38:51 +0530

    Added: UC 2: Retrieve Employee Payroll Data

--- src/main/java/org/example/PayrollService.java
+++ src/main/java/org/example/PayrollService.java
@@ -17,6 +17,10 @@ public class PayrollService {
             System.out.println("UC 1: Database Connection Test");
             payrollService.testDatabaseConnection();
 
+            System.out.println("\nUC 2: Retrieve Employee Payroll Data");
+            payrollService.retrieveEmployeePayrollData();
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,5 +34,23 @@ public class PayrollService {
             DriverManager.drivers().forEach(driver -> System.out.println("Driver: " + driver));
         }
 
+    }
+    // UC 2: Retrieve Employee Payroll Data
+    public void retrieveEmployeePayrollData() throws SQLException {
+        List<EmployeePayroll> employeePayrollList = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll")) {
+
+            while (resultSet.next()) {
+                EmployeePayroll employee = new EmployeePayroll(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("salary"),
+                        resultSet.getDate("start_date"));
+                employeePayrollList.add(employee);
+            }
+        }
+        employeePayrollList.forEach(System.out::println);
     }
 }
